a201a8698380ec65c57b58a901c007510a55a784
Voyage <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-04-21 12:14:17 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
      emacs
+     neovim
+     vscodium
      nitch
      hyfetch
      pfetch
@@ -123,6 +125,8 @@
      rustc
      cmake
      docker-client
+     
+     
      # podman
      #more utilites
      libressl
@@ -151,7 +155,7 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
+  # This value determines the NixOS / VynxOSrelease from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
