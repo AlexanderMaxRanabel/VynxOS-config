7a0f15d3dca30f2ce6f8e42242459ce52a36fd21
Voyage <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-08-07 13:53:20 +0300

    Optimized

--- configuration.nix
+++ configuration.nix
@@ -111,7 +111,6 @@
      emacs
      neovim
      vscodium
-     nitch
      hyfetch
      pfetch
      helix
@@ -125,9 +124,6 @@
      go
      rustup
      cmake
-     docker-client
-#  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -149,12 +145,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 
 }
